c2041268c657633cd41e8a79ebe364a5e9f282a0
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-03-15 17:11:48 +0800

    Added Routes for Home, About & Contact Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Main } from "./containers";
 
 function App() {
     return (
         <div>
-            <h1>Hello World</h1>
+            <Main />
         </div>
     );
 }
--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -1,7 +1,20 @@
 import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Navbar } from "../components";
+import { About, Contact, Home, Menu } from "../pages";
 
 function Main(props) {
-    return <div>Main</div>;
+    return (
+        <div>
+            <Navbar />
+            <Menu />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default Main;
