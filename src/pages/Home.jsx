6748ceac5ef39ff7bf82da45b0063da0b1116481
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-03-16 19:04:11 +0800

    Added Responsive Fonts for Mobile Devices for Title Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import React, { useState } from "react";
 function Home(props) {
     return (
         <div className="page-container">
-            Home
             <Box
                 sx={{
                     display: "flex",
@@ -15,9 +14,7 @@ function Home(props) {
                     flexDirection: "row",
                     "@media (max-width: 900px)": {
                         minHeight: "100%",
-                        padding: "3rem",
-                        justifyContent: "space-between",
-                        padding: "0rem",
+                        padding: "0",
                         gap: "1rem",
                     },
                     "@media (max-width: 650px)": {
@@ -31,24 +28,62 @@ function Home(props) {
                     className="title"
                     sx={{
                         zIndex: "1",
-                        "@media (max-width: 650px)": {
-                            marginTop: "-100px",
-                            // transform: "translate(0, -100px)",
+                        backgroundColor: "rgba(255, 255, 255, 0.8)",
+                        padding: "1rem",
+                        "@media (max-width: 900px)": {
+                            padding: "0rem",
                         },
+                        "@media (max-width: 650px)": {
+                            width: "auto",
+                            marginTop: "-100px",
+                            padding: "1rem",
+                        },
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "0.25rem",
                     }}
                 >
                     <h5>KEN YOKOHAMA</h5>
                     <h1>FULL STACK DEVELOPER</h1>
-                    <h3>
-                        Hello! I'm a <b>Web Developer</b> and licensed Civil
-                        Engineer with a passion for solving problems, learning
-                        new things and turning ideas into reality with the use
-                        of code.
+                    <h3 style={{ maxWidth: "600px" }}>
+                        Hello! I'm a <strong>Web Developer</strong> and licensed
+                        Civil Engineer with a passion for solving problems,
+                        learning new things and turning ideas into reality with
+                        the use of code.
                     </h3>
+                    <h4 style={{ marginTop: "1rem", fontWeight: "100" }}>
+                        <Box
+                            component="strong"
+                            sx={{
+                                transition: "border 0.5s ease",
+                                cursor: "pointer",
+                                border: "2px solid transparent",
+                                ":hover": {
+                                    borderBottom: "2px solid",
+                                },
+                            }}
+                        >
+                            View Projects
+                        </Box>{" "}
+                        or{" "}
+                        <Box
+                            component="strong"
+                            sx={{
+                                transition: "border 0.5s ease",
+                                cursor: "pointer",
+                                border: "2px solid transparent",
+                                ":hover": {
+                                    borderBottom: "2px solid",
+                                },
+                            }}
+                        >
+                            About Me
+                        </Box>
+                    </h4>
                 </Box>
                 <Box
                     sx={{
-                        width: "600px",
+                        width: "300px",
                         "@media(max-width: 900px)": {
                             width: "100%",
                         },
