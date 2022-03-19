4d1c7cdb7dc37439d02ca70ce9670c04a3e77d92
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-03-19 21:26:00 +0800

    Added Technologies Images and Grid Layout

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,29 @@ import { Box } from "@mui/material";
 import React, { useState } from "react";
 
 function About(props) {
+    const technologiesImgSrc = [
+        "html.png",
+        "css.png",
+        "javascript.png",
+        "react1.png",
+        "Firebase1.png",
+        "nodejs1.png",
+        "expressjs1.png",
+        "mongodb1.jpg",
+        "materialui2.png",
+        "framermotion1.png",
+        "react-router1.webp",
+        "git1.png",
+        "heroku1.png",
+        "netlify1.png",
+        "bootstrap2.png",
+        "tailwindcss.jpg",
+        "jquery1.png",
+        "npm1.png",
+        "autoit1.png",
+        "stackoverflow1.png",
+    ];
+
     return (
         <div
             className="page-container padding-for-nav"
@@ -15,8 +38,12 @@ function About(props) {
                 sx={{
                     maxWidth: "1600px",
                     paddingTop: "2rem",
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "10rem",
                 }}
             >
+                {/* ---------------------About Me Header------------------------ */}
                 <div
                     style={{
                         display: "flex",
@@ -43,6 +70,54 @@ function About(props) {
                         vero sunt tempore? Voluptates, doloribus totam.
                     </h6>
                 </div>
+                {/* ---------------------LANGUAGES SECTION------------------------ */}
+
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <h5>LANGUAGES</h5>
+                    <h2 style={{ textAlign: "center" }}>
+                        Some Technologies I'm fluent with
+                    </h2>
+                    <Box
+                        sx={{
+                            display: "grid",
+                            gridTemplateColumns: "1fr 1fr 1fr 1fr",
+                        }}
+                    >
+                        {technologiesImgSrc.map((img, index) => (
+                            <div key={index}>
+                                <img
+                                    style={{ maxWidth: "100%" }}
+                                    src={
+                                        "/images/about-page/technologies/" + img
+                                    }
+                                    alt={img}
+                                />
+                            </div>
+                        ))}
+                    </Box>
+                </div>
+                {/* ---------------------ENGINEERING & DESIGN SECTION------------------------ */}
+
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <h5>SOFTWARE</h5>
+                    <h2 style={{ textAlign: "center" }}>
+                        Engineering & Design
+                    </h2>
+                </div>
+
+                {/* ---------------------REFACTORING------------------------ */}
                 <div>
                     <h1>Technical Skills</h1>
                     {/*  */}
@@ -54,15 +129,19 @@ function About(props) {
                     <li>REACT</li>
                     <li>FIREBASE</li>
                     <li>NODE JS</li>
-                    <li>EXPRESS</li>
+                    <li>EXPRESSJS</li>
                     <li>MONGO DB</li>
                     <li>MATERIAL UI</li>
                     <li>FRAMER MOTION</li>
                     <li>REACT ROUTER DOM</li>
                     <li>GIT</li>
                     <li>GITHUB</li>
+                    <li>Heroku</li>
+                    <li>Netlify</li>
                     <li>BOOTSTRAP</li>
+                    <li>TAILWIND CSS</li>
                     <li>JQUERY</li>
+                    <li>NPM</li>
                     <li>AUTOIT</li>
                     <li>STACK OVERFLOW</li>
                     {/*  */}
