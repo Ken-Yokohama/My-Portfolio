d1047c4f90fdc5c731a62c78ab2c8726e1ce1514
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-03-21 22:55:38 +0800

    Edited Images in Carousel to contain arrows for better clarification

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -39,18 +39,11 @@ function About(props) {
     ];
 
     const educationImgSrc = [
-        "graduation1.jpg",
-        "graduation2.jpg",
+        "graduation1-arrow.png",
+        "graduation2-arrow.png",
         "golden1.png",
         "golden2.png",
         "golden3.png",
-    ];
-
-    const images = [
-        "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
-        "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80",
-        "https://media.istockphoto.com/photos/mountain-landscape-picture-id517188688?k=20&m=517188688&s=612x612&w=0&h=i38qBm2P-6V4vZVEaMy_TaTEaoCMkYhvLCysE7yJQ5Q=",
-        "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80",
     ];
 
     return (
