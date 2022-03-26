d3b7ba18eae49b91ee7dcc47aa3755b766e250ba
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-03-27 02:45:03 +0800

    Added github pages npm package and replaced browser router with hash router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 // Don't forget to replace BrowserRouter to HashRouter before deploying
 ReactDOM.render(
-    <BrowserRouter>
+    <Router>
         <App />
-    </BrowserRouter>,
+    </Router>,
     document.getElementById("root")
 );
