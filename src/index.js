4288513345cf7b103abffebc4382b6110494ceaf
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-04-05 17:35:26 +0800

    Added Browser Router Files to Fix Routing upon Refresh

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 // Don't forget to replace BrowserRouter to HashRouter before deploying
 ReactDOM.render(
-    <Router>
+    <Router basename="/">
         <App />
     </Router>,
     document.getElementById("root")
