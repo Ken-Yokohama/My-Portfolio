771b9aa8893b9739fa98f96a996bc3bba6100c72
Ken <kenlopezyokohama@gmail.com>
2023-06-20 23:17:43 +0800

    Refactored project data for Home Page

---
+++ src/pages/home/project-data.ts
@@ -0,0 +1,111 @@
+export const featuredProjects = [
+    {
+        year: "2022",
+        month: "MAR",
+        name: "Twitter Clone",
+        description:
+            "Twitter Clone is a full-stack social media web application similar to the popular website Twitter created using React in the front end and Firebase to handle the Database, Authentication & Security.",
+        technologies: [
+            "REACT",
+            "FIREBASE",
+            "MATERIAL UI",
+            "REACT ROUTER",
+            "BAD WORDS",
+        ],
+        liveSiteLink: "https://ken-yokohama.github.io/Twitter-Clone",
+        githubRepoLink: "https://github.com/Ken-Yokohama/Twitter-Clone",
+        backgroundImgUrl:
+            "/images/home-page/project-backgrounds/twitter-clone.JPG",
+    },
+    {
+        year: "2022",
+        month: "FEB",
+        name: "Kenterest",
+        description:
+            "Kenterest is a full-stack social media photo sharing web application similar to the popular website Pinterest created using React in the Frontend and Sanity.io as the CMS.",
+        technologies: [
+            "REACT",
+            "SANITY.IO",
+            "TAILWIND CSS",
+            "REACT ROUTER",
+            "UUID",
+        ],
+        liveSiteLink: "https://kenterest.netlify.app/",
+        githubRepoLink: "https://github.com/Ken-Yokohama/Kenterest",
+        backgroundImgUrl: "/images/home-page/project-backgrounds/kenterest.JPG",
+    },
+    {
+        year: "2022",
+        month: "Apr",
+        name: "Bug Tracker",
+        description:
+            "This Bug Tracking System is a full-stack software issue management web application that allows developers to create tickets for their projects and admins to manage the progress of each project using NodeJS & Express in the back end, MongoDB as the database and React in the front end",
+
+        technologies: [
+            "MongoDB",
+            "ExpressJS",
+            "React",
+            "NodeJS",
+            "Redux",
+            "Typescript",
+        ],
+        liveSiteLink: "https://ken-yokohama.github.io/Bug-Tracking-System",
+        githubRepoLink: "https://github.com/Ken-Yokohama/Bug-Tracking-System",
+        backgroundImgUrl:
+            "/images/home-page/project-backgrounds/bug-tracker.jpg",
+    },
+    {
+        year: "2022",
+        month: "MAR",
+        name: "Ka-Chat!",
+        description:
+            "Ka-Chat! is a full-stack realtime chat application created using React in the Frontend and Firebase as the CMS.",
+        technologies: [
+            "TYPESCRIPT",
+            "REACT",
+            "FIREBASE",
+            "MATERIAL UI",
+            "REACT ROUTER",
+        ],
+        liveSiteLink: "https://ken-yokohama.github.io/Ka-Chat/",
+        githubRepoLink: "https://github.com/Ken-Yokohama/Ka-Chat",
+        backgroundImgUrl: "/images/home-page/project-backgrounds/ka-chat.JPG",
+    },
+];
+
+export const projects = [
+    {
+        title: "MailChimp API Newsletter Sign Up Page",
+        technologies: ["nodeJS", "expressJS", "mailChimp API"],
+        description:
+            "This is a web application that uses the MailChimp API to subscribe users to my personal mailing list. In the Mailchimp database, we are able to organize each user and filter which content we want to send for individual users. Additionally, using the MailChimp api allows us to sync all data (subscriber, customers, orders, products) and enables marketing automation with email campaigns, automations, ads, postcards and more.",
+        liveSite: "https://still-garden-96667.herokuapp.com/",
+        githubRepo:
+            "https://github.com/Ken-Yokohama/Node-Express-JS-Training-Files#mailchimp-api-newsletter-signup",
+    },
+    {
+        title: "EJS Express To Do List App",
+        technologies: ["nodeJS", "expressJS", "Mongo DB", "EJS"],
+        description:
+            "This is a simple web application that uses MongoDB as the database and EJS for templating as well as partials for components. The frontend displays the current date & month at the top. Input feilds allows users to input any tasks they have for the given day. Users can also crossout any tasks that have been completed.",
+        liveSite: "https://to-do-list-using-mongodb-atlas.herokuapp.com/",
+        githubRepo:
+            "https://github.com/Ken-Yokohama/Node-Express-JS-Training-Files#ejstodoist-v1",
+    },
+    {
+        title: "PSBSI",
+        technologies: ["HTML", "CSS", "JS", "JQUERY", "BOOTSTRAP"],
+        description:
+            "This is the landing page for the company Pioneer Specialty Building Systems Inc. The website displays all completed & upcoming projects done by the company as well as details on its building systems such as Waterproofing, Eifs & Sips.",
+        liveSite: "https://ken-yokohama.github.io/psbsi/",
+        githubRepo: "https://github.com/Ken-Yokohama/psbsi",
+    },
+    {
+        title: "Tindog",
+        technologies: ["HTML", "CSS", "JS", "BOOTSTRAP"],
+        description:
+            "This is a simple static webpage used as a parody of Tinder... But for Dogs! This project was mainly used to learn basic CSS design patterns as well as responsive design using the Bootstrap grid system.",
+        liveSite: "https://ken-yokohama.github.io/TinDog/",
+        githubRepo: "https://github.com/Ken-Yokohama/TinDog",
+    },
+];
